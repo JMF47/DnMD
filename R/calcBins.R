76138fc4a0090bd6d9729ea9c5841637fccbbafa
Jack Fu <jmfu@jhsph.edu>
2018-05-16 07:33:31 -0400

    Revert "Update calcBins.R"
    This reverts commit 07fc31df63e1212209010c6ebb20b1ff612cc822.

--- R/calcBins.R
+++ R/calcBins.R
@@ -125,21 +125,31 @@ calcBins <- function(pD, n, rl, med, min, genome, map_file, seed=1337){
 .extractCounts = function(cov_list){
       return(suppressWarnings((as.numeric(unlist(cov_list)))))
 }
-.divideSegs <- function(seg, covs, rl, med){
-      cov_sub <- lapply(covs, function(x) x[seg])
-      cov_sub_mat <- do.call(rbind, lapply(cov_sub, .extractCounts))
-      cov_sub_cs_normed <- t(apply(cov_sub_mat, 1, cumsum))/rl
-      medz <- apply(cov_sub_cs_normed, 2, stats::median)
-      medzCut <- floor(medz/med)
-      changePoint <- vapply(seq_len(max(medzCut)), function(x){
-            min(which(medzCut==x))}, integer(1))
-      changePoint <- c(0, changePoint)
-            changePoint[length(changePoint)] <- width(seg)
-      gr_out <- GRanges(seqnames=seqnames(seg),
-            IRanges(start(seg)+changePoint[-length(changePoint)],
-            start(seg)+changePoint[-1]-1))
-      gr_medz <- medz[changePoint]
-      gr_out$median_count <- gr_medz-med*(seq_len(length(gr_medz))-1)
+.divideSegs = function(seg, covs, rl, med){
+      output = NULL
+      j = 1
+      count = 1
+      reads_remain = seg$read
+      num_segs = floor(seg$reads/med)
+      cov_sub = lapply(covs, function(x) x[seg])
+      cov_sub_mat = do.call(rbind, lapply(cov_sub, .extractCounts))
+      
+      cov_sub_cs_normed = t(apply(cov_sub_mat, 1, cumsum))/rl
+      medz = apply(cov_sub_cs_normed, 2, stats::median)
+      while(max(medz)>=med){
+            cut = which(medz>=med)[1]
+            output = rbind(output, c(cut, medz[cut]))
+            count = count+1; j = sum(output[,1])+1
+            cov_sub_cs = t(apply(cov_sub_mat[,j:dim(cov_sub_mat)[2],drop=FALSE], 1, cumsum))
+            cov_sub_cs_normed = cov_sub_cs/rl
+            medz = apply(cov_sub_cs_normed, 2, stats::median)
+      }
+      output = rbind(output, c(width(seg)-sum(output[,1]), max(medz)))
+
+      cs = cumsum(output[,1])
+      coords = cbind(c(0, cs[-length(cs)]), cs-1)
+      gr_out = GRanges(seqnames=seqnames(seg), IRanges(start(seg)+coords[,1], start(seg)+coords[,2]))
+      gr_out$median_count = output[,2]
       return(gr_out)
 }
 .calcGC = function(biostring){
